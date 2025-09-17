1a3a512745b975d9bfe2ec7465297372dfebdcb6
Manas Ranjan Jena <mranjanjena253@gmail.com>
2025-09-17 20:59:57 +0530

    Updated Readme

--- backend/ML_models/dl_model_for_img_panic_meter.py
+++ backend/ML_models/dl_model_for_img_panic_meter.py
@@ -51,7 +51,7 @@ train_transform = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.RandomHorizontalFlip(),
     transforms.ColorJitter(brightness = 0.2, contrast = 0.2),
-    transforms.TrivialAugmentWide(num_magnitude_bins = 15),
+    transforms.TrivialAugmentWide(num_magnitude_bins = 25),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
@@ -66,52 +66,55 @@ train_subset = torch.utils.data.Subset(dataset, train_idx)
 val_subset = torch.utils.data.Subset(dataset, val_idx)
 
 BATCH_SIZE = 64
-train_loader = DataLoader(train_subset, batch_size = BATCH_SIZE, shuffle = True)
-val_loader = DataLoader(val_subset, batch_size = BATCH_SIZE, shuffle = False)
+train_loader = DataLoader(train_subset, batch_size = BATCH_SIZE, shuffle = True, pin_memory = True, persistent_workers = True, num_workers = 6)
+val_loader = DataLoader(val_subset, batch_size = BATCH_SIZE, shuffle = False, pin_memory = True, persistent_workers = True, num_workers = 6)
 
 # Training the model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-# model = models.resnet18(pretrained = True)
-# model.fc = nn.Linear(model.fc.in_features, 1)   # regression head
-model = PanicMeterModel(img_size = 256)
+model = models.resnet18(pretrained = True)
+model.fc = nn.Linear(model.fc.in_features, 3)   # regression head
+# model = PanicMeterModel(img_size = 256)
 model = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = 1e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr = 1e-4, weight_decay = 1e-4)
 
+num_epochs = 200
 
-num_epochs = 500
+def train():
+    for epoch in tqdm(range(num_epochs)):
+        # Training
+        model.train()
+        train_loss = 0
+        for imgs, labels in train_loader:
+            imgs, labels = imgs.to(device, non_blocking = True), labels.to(device, non_blocking = True)
 
-for epoch in tqdm(range(num_epochs)):
-    # Training
-    model.train()
-    train_loss = 0
-    for imgs, labels in train_loader:
-        imgs, labels = imgs.to(device), labels.to(device)
-
-        optimizer.zero_grad()
-        outputs = model(imgs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()
-
-    # Validation
-    model.eval()
-    val_loss = 0
-    with torch.no_grad():
-        for imgs, labels in val_loader:
-            imgs, labels = imgs.to(device), labels.to(device)
+            optimizer.zero_grad()
             outputs = model(imgs)
             loss = criterion(outputs, labels)
-            val_loss += loss.item()
-    if epoch % 50 == 0 or epoch + 1 == num_epochs:
-        print(f"Epoch [{epoch+1}/{num_epochs}] "
-              f"Train Loss: {train_loss/len(train_loader):.4f} "
-              f"Val Loss: {val_loss/len(val_loader):.4f}")
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
 
-# Saving the model
-torch.save(model.state_dict(), "models/panic_level_custom_model.pth")
-print("Model saved successfully.")
+        # Validation
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for imgs, labels in val_loader:
+                imgs, labels = imgs.to(device, non_blocking = True), labels.to(device, non_blocking = True)
+                outputs = model(imgs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+        if epoch % 20 == 0 or epoch + 1 == num_epochs:
+            print(f"Epoch [{epoch+1}/{num_epochs}] "
+                  f"Train loss: {train_loss/len(train_loader):.4f} "
+                  f"Val loss: {val_loss/len(val_loader):.4f}")
+
+    # Saving the model
+    torch.save(model.state_dict(), "models/panic_level_custom_model.pth")
+    print("Model saved successfully.")
+
+if __name__ == "__main__":
+    train()
