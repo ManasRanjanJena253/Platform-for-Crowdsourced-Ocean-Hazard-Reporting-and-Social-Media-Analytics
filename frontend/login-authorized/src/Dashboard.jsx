d521730988e2aaaa72b0b80c76d91429da780781
Raghav <raghav5rastogi2005@gmail.com>
2025-09-19 01:11:13 +0530

    Added Frontend Part

---
+++ frontend/login-authorized/src/Dashboard.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+// export default function Dashboard() {
+//   return (
+//     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+//       <h1>Welcome to your Dashboard 🎉</h1>
+//       <p>This page is protected and only visible after login.</p>
+//     </div>
+//   );
+// }
